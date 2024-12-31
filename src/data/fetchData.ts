05992b153b3df6595e3c5f4ef31268332380163a
Felix Dumas-Lavoie <felixdumaslavoie@gmail.com>
2024-12-31 09:41:42 -0500

    Ca s en vient.

--- src/data/fetchData.ts
+++ src/data/fetchData.ts
@@ -1,7 +1,69 @@
-import textsData from "../content/data/data.json";
+import type { Url } from "node:url";
+import data from "../content/data/data.json";
+
+function splitUrl(url: URL) {
+  if (url.pathname) {
+    let splitted = url.pathname.split('/')
+    return splitted
+  }
+  return null;
+}
 
 
-export function findPageId(url: URL) {
+export function findPageId(url: URL): number {
 
-  console.log("ALLO" + " " + url.pathname)
+  let split = splitUrl(url)
+
+  if (split) {
+    console.log(!isNaN(Number(split[2])))
+    if (!isNaN(Number(split[2]))) // Si mainpage
+    {
+      return Number(split[2])
+    }
+  }
+
+  return 0
+}
+
+const Languages = {
+  Fr: "fr",
+  En: "en",
+}
+
+export function pageLang(url: URL): String {
+  let split = splitUrl(url)
+
+  if (split) {
+    console.log(split[1])
+
+    if (Object.values(Languages).includes(split[1])) {
+      return String(split[1])
+    }
+
+    throw new Error("Invalid url (fetchData / pageLang)")
+  }
+
+  return "fr"
+}
+
+function switchLang(lang: String) {
+  return lang === "fr" ? "en" : "fr";
+}
+
+export function translatedText(url: URL): URL {
+
+  let currentId = findPageId(url)
+  let currentLang = pageLang(url)
+  let newLang = switchLang(currentLang)
+  //console.log(currentId)
+
+  let query = data["id2url"][currentId][newLang]
+
+  if (query) {
+    //console.log(query)
+  }
+
+  return url;
+
+
 }
