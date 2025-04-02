70d275e8d17d7ba79f827a92b84cb35cddc5b0f0
Félix Dumas-Lavoie <felixdumaslavoie@gmail.com>
2025-04-02 13:34:14 -0400

    encore d autres petites modifs.

--- src/content/archives/cute/en/swuc_archives.md
+++ src/content/archives/cute/en/swuc_archives.md
@@ -3,4 +3,11 @@ modification: 2025-03-06T17:19:39.000Z
 title: SWUC Archives
 era: 2016-2019
 excerpt: Between 2016 and 2019, a campaign for the recognition of work done in a student environment is led by the CUTE (Comités unitaires sur le travail étudiant).
+material:
+  magazines:
+
+  tracts:
+  affiches:
+  videos:
+  sites web:
 ---
--- src/content/config.ts
+++ src/content/config.ts
@@ -15,4 +15,21 @@ const blog = defineCollection({
   }),
 });
 
+
+const archives = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    modification: z.coerce.date(),
+    title: z.string(),
+    excerpt: z.string(),
+    authors: z.array(z.string()),
+    editors: z.array(z.string()),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    heroImage: z.string(),
+  }),
+});
+
+
 export const collections = { blog };
