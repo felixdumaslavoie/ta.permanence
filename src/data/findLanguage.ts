46bbcb7baacdfc1efddcc7377eee7ad007441a58
Félix Dumas-Lavoie <felixdumaslavoie@gmail.com>
2025-06-10 21:01:49 -0400

    correctif de la langue de la date.

--- src/data/findLanguage.ts
+++ src/data/findLanguage.ts
@@ -1,15 +1,11 @@
 import { findPageId } from "./fetchData";
 
 // URL en entrée --> PROCESSING --> Langue du texte en question en sortie.
-function findPageLanguage() {
-
-  return 'fr';
-}
-
-
 export function findLanguage(url: URL): string {
-  const idStr = findPageId(url);
 
-  return findPageLanguage();
+  let stringUrl = url.pathname
+
+
+  return stringUrl.split(`/`)[1];
 }
 
