382d2b34d387a307adb4a57dc6be248c50edbc6a
Félix Dumas-Lavoie <felixdumaslavoie@gmail.com>
2025-07-08 11:45:56 -0400

    Travaux chiants sur le print mode.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: "class",
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('daisyui'),
-		require('tailwind-hamburgers')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: { print: { raw: "print" }, screen: { raw: "screen" } },
+  },
+  plugins: [
+    require("daisyui"),
+    require("tailwind-hamburgers"),
+  ],
+};
