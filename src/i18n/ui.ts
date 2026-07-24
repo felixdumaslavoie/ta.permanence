8957c63ae5e9d314d489f29837f625415e95281d
felixdumaslavoie <felixdumaslavoie@gmail.com>
2026-07-24 13:47:18 -0400

    Plus de textes.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,7 +3,7 @@ import { SITE_TITLE, SITE_DESCRIPTION, SOCIALS_MEDIA } from '../consts'
 
 export const defaultLang = 'fr';
 
-export const textsByMainPage: number = 3
+export const textsByMainPage: number = 25
 
 export const languages = {
   en: 'English',
